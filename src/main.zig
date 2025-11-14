714b5f8ec9cdd44d25078e35911ca4906c744c69
Gilkleber Medeiros de Araujo <20252100450017@ifrn.local>
2025-11-14 20:31:26 -0300

    parsed string into zig-clap cli params.

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
-const clap = @import("zig-clap");
+const clap = @import("clap");
 
 pub fn main() !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const params = comptime clap.parseParamsComptime("\n\n-h, --help                  Displays this help and exit.\n--ignore-[listed command]    Ignore a listed command given its name.\n--o-[git add option]        Use git add options when command executed (optional).\n");
+
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
