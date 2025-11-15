1c01291e96a62c7a81c4e80fbcb80f2cd233feb9
Gilkleber Medeiros <gikimedei2018@gmail.com>
2025-11-15 14:36:51 -0300

    fix: successfull extract command line params.

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,86 @@
 const std = @import("std");
 const clap = @import("clap");
 
+const ProgramParams = struct {
+    paths: []const []const u8,
+    help: bool = undefined,
+    version: bool = undefined,
+    ignore: ?[]const []const u8 = undefined,
+    options: ?[]const []const u8 = undefined,
+
+    pub fn init(
+        paths: []const []const u8,
+        help: ?bool,
+        version: ?bool,
+        ignore: ?[]const []const u8,
+        options: ?[]const []const u8,
+    ) ProgramParams {
+        return ProgramParams{
+            .paths = paths,
+            .help = help orelse false,
+            .version = version orelse false,
+            .ignore = ignore orelse null,
+            .options = options orelse null,
+        };
+    }
+};
+
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    const params = comptime clap.parseParamsComptime("\n\n-h, --help                  Displays this help and exit.\n--ignore-[listed command]    Ignore a listed command given its name.\n--o-[git add option]        Use git add options when command executed (optional).\n");
+    const params = comptime clap.parseParamsComptime("\n\n-h, --help                  Displays this help and exit.\n-v, --version               Displays program version.\n-i, --ignore <str>...           Ignore a listed command given its name.\n-o, --options <str>...      Use git add options.\n<str>...                    Paths to be passed to actual git add command.\n");
+
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = gpa.allocator(),
+    }) catch |err| {
+        // Report useful error and exit.
+        try diag.reportToFile(.stderr(), err);
+        return err;
+    };
+    defer res.deinit();
+
+    const allocator = gpa.allocator();
+    const programParams: *ProgramParams = try allocator.create(ProgramParams);
+    defer allocator.destroy(programParams); // deallocate when exit this scope
+
+    handleParams(res, programParams);
+
+    std.debug.print("ProgramParams: {any} \n", .{programParams});
+    std.debug.print("Paths: {any} \n", .{programParams.paths});
+    std.debug.print("Options: {any} \n", .{programParams.options});
+    std.debug.print("Ignore: {any} \n", .{programParams.ignore});
+    std.debug.print("Help: {any} \n", .{programParams.help});
+    std.debug.print("Version: {any} \n", .{programParams.version});
 
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+}
+
+// @Requires res = clap.Result(Help)
+pub fn handleParams(res: anytype, paramsStruct: *ProgramParams) void {
+    if (res.positionals[0].len == 0) {
+        @panic("Required param '<path>...' (git add <path>...) not found! exiting...");
+    }
+
+    paramsStruct.* = ProgramParams.init(res.positionals[0], false, false, null, null);
+
+    if (res.args.help != 0) {
+        paramsStruct.help = true;
+        // show help
+    }
+    if (res.args.version != 0) {
+        paramsStruct.version = true;
+        // show version
+    }
+    if (res.args.ignore.len > 0) {
+        paramsStruct.ignore = res.args.ignore;
+        // set ignore
+    }
+    if (res.args.options.len > 0) {
+        paramsStruct.options = res.args.options;
+        // set git add options
+    }
 }
